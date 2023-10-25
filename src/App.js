2ee3a9d5f050727eebb4956f797fb261fe3bed1a
Eleven <danielulelu@gmail.com>
2023-10-25 10:20:05 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const name = 'Daniel';
+  return (
+    <div className="App">
+      <div className="content">
+        Goodmorning {name}
+      </div>
+    </div>
+  );
+}
+
+export default App;
