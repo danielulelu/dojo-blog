546674835f6ef661d16f4a98a98a8552df1ba2c1
Eleven <danielulelu@gmail.com>
2023-10-26 08:38:33 +0300

    added useFetch hook to customize and destructure the homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
-  const name = 'Daniel';
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        Goodmorning {name}
+        <Home />
       </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import BlogList from "../components/BlogList";
+// import { useState, useEffect } from "react";
+import useFetch from "../useFetch";
+
+const Home = () => {
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+      { isPending && <div>{error}</div>}
+      {isPending && <div style={{ color: 'green', textDecoration: 'underline'  }}>Give me a second</div>}
+      {/* Conditional template in react */}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
+};
+export default Home;
